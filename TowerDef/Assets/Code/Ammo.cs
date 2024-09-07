caf01aa40d979158ed488958aeaa6f26b163fec7
Nkwemakaleng <nkwemakaleng@gmail.com>
2024-09-07 07:48:02 +0200

    Tower
    tower code 90%

---
+++ TowerDef/Assets/Code/Ammo.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform target;
+    public float ammoSpeed = 70f;
+    public void Track(Transform _target)
+    {
+        target = _target;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+                return;
+        }
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = ammoSpeed * Time.deltaTime;
+        if (dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+    }
+
+    private void HitTarget()
+    {
+        Debug.Log("Im shot");
+    }
+}
---
+++ TowerDef/Assets/Code/TowerHandler.cs
@@ -0,0 +1,118 @@
+using System;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public event TowerDeath OnDeath;
+    
+    public delegate void TowerDeath();
+    public string enemyTag = "Enemy";
+   
+    private Transform target;
+    public Transform partRotate;
+    [Header("Stats")]
+    public float range;
+    public int health = 100;
+    public float rotateSpeed = 10f;
+    public float attackSPeed = 1f;
+    public float attackCooldown = 0f;
+    
+    public GameObject ammoType;
+    public Transform firePoint;
+    
+    
+    // Function to deal damage to the tower
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    // Function to handle the tower's death
+    private void Die()
+    {
+        Debug.Log(name + " has been destroyed.");
+        OnDeath?.Invoke();
+        Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f,0.5f);
+    }
+
+    private void UpdateTarget()// updates the 
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (var enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <=range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null; 
+        }
+    }
+
+    void Update()
+    {
+        if (target == null)
+        {
+            return;
+        }
+        TargetLockOn();
+        /*if (attackCooldown <= 0f)
+        {
+            attackCooldown = 1f / attackSPeed;
+        }
+        attackCooldown -= Time.deltaTime;*/
+    }
+
+    private void Shoot()
+    {
+       GameObject ammoGO = (GameObject)Instantiate(ammoType, firePoint.position, firePoint.rotation);
+       Ammo ammo = ammoGO.GetComponent<Ammo>();
+       if (ammo != null)
+       {
+           ammo.Track(target);
+       }
+    }
+    private void TargetLockOn()
+    {
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 actualRot = Quaternion.Lerp(partRotate.rotation, lookRotation, Time.deltaTime* rotateSpeed).eulerAngles;
+        partRotate.rotation = Quaternion.Euler(0f, actualRot.y,0f);
+        if (attackCooldown <= 0f)
+        {
+            Shoot();
+            attackCooldown = 1f / attackSPeed;
+        }
+        attackCooldown -= Time.deltaTime;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position,range);
+    }
+    
+    
+}
+
